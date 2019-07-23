cb17ca9c01cecd105d3f23443d9399d1975c4c8b
nick_richardson <nick.richardson@rezonence.com>
2019-07-23 12:02:05 +0100

    Test genesis block creation

--- spec/block.creator.ts
+++ spec/block.creator.ts
@@ -2,6 +2,7 @@ import {expect} from "chai";
 import {describe} from "mocha";
 import {Block} from "../src/block";
 import {BlockCreator} from "../src/block.creator";
+import {CreateBlockRequest} from "../src/create.block.request";
 import {HashCalculator} from "../src/hash.calculator";
 import {Logger} from "../src/logger";
 
@@ -27,11 +28,23 @@ describe("Block creator", () => {
     });
 
     it("Makes new blocks", async () => {
-        difficulty = 3;
-        const request = { lastBlock: mockBlock, data: "baz", difficulty };
-        const newBlock = await creator.createNewBlock(request);
+        let newBlock: Block;
+        let request: CreateBlockRequest;
+        difficulty = 2;
+        request = { lastBlock: mockBlock, data: "baz", difficulty };
+        newBlock = await creator.createNewBlock(request);
         expect(newBlock.index).to.eql(1);
         expect(newBlock.data).to.eql("baz");
+        expect(newBlock.hash.startsWith("00")).to.eql(true);
+        difficulty = 3;
+        request = { lastBlock: mockBlock, data: "baz", difficulty };
+        newBlock = await creator.createNewBlock(request);
         expect(newBlock.hash.startsWith("000")).to.eql(true);
     });
+
+    it("Makes genesis blocks", () => {
+        const genesisBlock = creator.createGenesisBlock();
+        expect(genesisBlock.index).to.eql(0);
+        expect(genesisBlock.data).to.eql(creator.genesisData);
+    })
 });
