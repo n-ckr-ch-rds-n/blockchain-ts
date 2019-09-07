583edcfb8a55bfab250335267fe8cbce62c8f700
Nick <nick.richardson@rezonence.com>
2019-09-07 15:39:19 +0100

    Test validation of genesis blocks

--- spec/block.chain.validator.spec.ts
+++ spec/block.chain.validator.spec.ts
@@ -1,3 +1,6 @@
+import {expect} from "chai";
+import {describe} from "mocha";
+import {Block} from "../src/block";
 import {BlockChainValidator} from "../src/block.chain.validator";
 import {FileService} from "../src/file.service";
 import {HashCalculator} from "../src/hash.calculator";
@@ -8,25 +11,35 @@ describe("Blockchain validator", () => {
     let mockFileService: FileService;
     let mockHashCalculator: HashCalculator;
     let mockLogger: MockLogger;
+    let mockGenesisBlock: Block;
+    let mockGenesisBlockHash: string;
 
     beforeEach(() => {
+        mockGenesisBlockHash = "foo";
+        mockGenesisBlock = {
+            hash: mockGenesisBlockHash,
+            index: 0,
+            nonce: 0,
+            prevHash: "bar",
+            timeStamp: "baz",
+            data: "foobar"
+        };
         mockFileService = {
             getChain: (fileName: string) => ({
                 difficulty: 0,
-                chain: [{
-                    hash: "foo",
-                    index: 0,
-                    nonce: 0,
-                    prevHash: "bar",
-                    timeStamp: "baz",
-                    data: "foobar"
-                }]
+                chain: [mockGenesisBlock]
             })
         } as FileService;
         mockHashCalculator = {
-            calculateHash: () => "foobar"
+            calculateHash: () => mockGenesisBlockHash
         };
         mockLogger = new MockLogger();
         blockchainValidator = new BlockChainValidator(mockFileService, mockHashCalculator, mockLogger);
+    });
+
+    it("Checks whether genesis blocks are valid", () => {
+        expect(blockchainValidator.isValid(mockGenesisBlock)).to.eql(true);
+        mockHashCalculator.calculateHash = () => "bar";
+        expect(blockchainValidator.isValid(mockGenesisBlock)).to.eql(false);
     });
 });
